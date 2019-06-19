acba017ed00386d349eb510ee2a63cb30ca4d4dd
wolflikai <wolflikai@163.com>
2019-06-19 09:25:47 +0800

    kill process

---
+++ KillProcess/main.py
@@ -0,0 +1,36 @@
+# !/usr/bin/python3
+# -*- coding:utf-8 -*-
+'''
+    @name: main
+    @author: Administrator
+    @time: 2019/6/19 9:23
+    @email: wolflikai@163.com
+    @detail: ''
+'''
+
+"""
+根据名字杀进程 可杀多进程应用
+"""
+
+import subprocess
+import sys
+
+
+def kill_all(app_name):
+    cmd = 'ps -e | grep "%s" | grep -v grep | awk \'{print $1}\'' % app_name
+    ret = subprocess.check_output(cmd, shell=True)
+    if ret:
+        for line in ret.decode().split():
+            ret = subprocess.run(f'kill -9 {line}', shell=True)
+            if ret.returncode == 0:
+                print(f'kill {line}')
+    print('kill all finished')
+
+if __name__ == '__main__':
+    app_name = None
+    if len(sys.argv) == 2:
+        app_name = sys.argv[1]
+    if app_name:
+        kill_all(app_name)
+    else:
+        sys.exit()
